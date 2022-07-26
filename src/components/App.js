600bc7726ca20a31878e4520b5319546901515ee
OKEREKE VINCENT <Vincentcode0@gmail.com>
2022-07-26 09:49:31 +0100

    Fix bug

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,38 @@
-import './App.css';
 import { Component } from 'react';
 // import Popular from './Popular'
-// import Battle from './Battle';
-import DynamicRouting from './tutorials/DynamicRouting';
+import Battle from './Battle';
+// import DynamicRouting from './tutorials/DynamicRouting';
 // import Tutorial from '../components/tutorials/Tutorial'
 // import IMG from '../asset/img-2.jpg'
-
+import './App.css';
+import { ThemeProvider } from '../contexts/theme';
+import Nav from './Nav';
 class App extends Component {
-  state = {  } 
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       theme: 'light',
+       toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === 'light' ? 'dark' : 'light'
+        }))
+       }
+    }
+  }
+  
   render() { 
       return (
-          <div className="container">
-                <DynamicRouting />
-                {/* <Popular /> */}
+        <ThemeProvider value={this.state}>
+          <div className={this.state.theme}>
+            <div className="container">
+              <Nav />
+              {/* <DynamicRouting /> */}
+              <Battle />
               {/* <Tutorial user={{ name: 'Jane Doe', username: 'janedoe', img: IMG }} /> */}
+            </div>
           </div>
+        </ThemeProvider>
       );
   }
 }
